f9ffc051e6ecf4439dec91769ffa40cb9759a3f3
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-05-21 15:49:30 +0200

    Home-update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
+export const Home = () => {
+  const [journey, setJourney] = useState(null);
 
-export const Home = () => (
-  <main>
-    <JourneyPicker />
-  </main>
-);
+  const handleJourneyChange = (results) => {
+    setJourney(results);
+  };
+
+  return (
+    <main>
+      <JourneyPicker onJourneyChange={handleJourneyChange} />
+      <div className="container">
+        {journey === null ? null : (
+          <p>Nalezeno spojení s id {journey.journeyId}</p>
+        )}
+      </div>
+    </main>
+  );
+};
