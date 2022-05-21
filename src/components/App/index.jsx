21178f0afba71827480b97b30352c31d879dffc4
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-05-21 11:17:52 +0200

    Zalozeni aplikace

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reservation" element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
   </>
 );
